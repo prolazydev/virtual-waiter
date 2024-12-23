980573b5edf37ad1a84f6b62fb33e92624669ec5
Flamur <flamur.biz@hotmail.com>
2024-12-23 13:43:31 +0000

    Cleanup: Remove TODO comments and unused restaurant rating files

--- server/src/utils/common/index.ts
+++ server/src/utils/common/index.ts
@@ -28,6 +28,54 @@ export function regexFuzzySearch(text: string) {
 	return new RegExp('^' + regex, 'gi');
 }
 
+/**
+ * Merges two objects
+ * @param defaultObj Initial object
+ * @param mergingObj Merging object
+ * @returns Merged object
+ * @throws {Error} If the merging object contains mismatched properties
+ */
+export const mergeSameObjects = (defaultObj: object, mergingObj: object) => {
+	const merged = { ...defaultObj };
+
+	// TODO: Add property validation, if the mergingObj contains 1 or more mismatched properties, throw an error
+	const defaultKeys = Object.keys(defaultObj);
+	const mergingKeys = Object.keys(mergingObj);
+	const diff = defaultKeys.filter(key => !mergingKeys.includes(key));
+	if (diff.length > 0) 
+		throw new Error(`Merging object contains mismatched properties: ${diff.join(', ')}`);	
+	
+	for (const key in mergingObj) 
+		if (Object.prototype.hasOwnProperty.call(mergingObj, key)) 
+			// @ts-expect-error typing issue
+			merged[key] = mergingObj[key];
+
+	return merged;
+};
+
+/**
+ * Merges two objects of the same type
+ * @param defaultObj Initial object
+ * @param mergingObj Merging object
+ * @returns Merged object
+ * @throws {Error} If the merging object contains mismatched properties
+ */
+export const mergeSameTypedObjects = <TObject extends object>(defaultObj: TObject, mergingObj: TObject) => {
+	const merged = { ...defaultObj };
+
+	const defaultKeys = Object.keys(defaultObj);
+	const mergingKeys = Object.keys(mergingObj);
+	const diff = defaultKeys.filter(key => !mergingKeys.includes(key));
+	if (diff.length > 0) 
+		throw new Error(`Merging object contains mismatched properties: ${diff.join(', ')}`);
+
+	for (const key in mergingObj)
+		if (Object.prototype.hasOwnProperty.call(mergingObj, key)) 
+			merged[key] = mergingObj[key];
+
+	return merged;
+}
+
 // PRIVATE
 // function escapeRegex(text: string) {
 // 	return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
