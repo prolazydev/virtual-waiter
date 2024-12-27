2e614bb740dd471add239e0c481ca6c9c44686af
Flamur Fazliu <flamur.biz@hotmail.com>
2024-12-27 19:06:15 +0100

    Adde the unstaged component meant for the previus commit

--- client/src/components/ui/EditFormPart.vue
+++ client/src/components/ui/EditFormPart.vue
@@ -1,25 +1,35 @@
 <template>
-    <div class="flex flex-col" :title="title">
-        <label 
-            @click="emit('toggleEdit')" 
-            :for="id"
-            class="form-part-label" 
-        >
-            {{ title }}
-            <button type="button">
-				<LucideIcon 
-					:class="{ 'form-part-edit-icon-toggle': state === 'edit' }"
-					class="form-part-edit-icon"
-					name="PencilLine" 
-					size="24"
-				/>
-			</button>
-        </label>
+    <div class="flex flex-col" :title="`Toggle edit for ${title}`">
+		<label 
+			@click="emit('toggleEdit')" 
+			:for="id"
+			class="form-part-label" 
+		>
+			{{ title }}
+			<Tooltip 
+				:text="`Toggle edit for ${title}`" 
+				:show="true" 
+				:delay="1000"
+				_class="mb-3"
+			>
+				<button type="button">
+					<LucideIcon 
+						:class="{ 'form-part-edit-icon-toggle': state === 'edit' }"
+						class="form-part-edit-icon"
+						name="PencilLine" 
+						size="24"
+					/>
+				</button>
+			</Tooltip>
+		</label>
         
         <slot></slot>
 
         <div class="min-h-14 relative transition-all">
-            <div class="w-full h-[1px] bg-black absolute transition-all ease" :class="{ 'top-full': state === 'edit', 'top-10': state === 'idle' }"></div>
+            <div 
+				:class="{ 'top-full': state === 'edit', 'top-10 delay-150': state === 'idle' }"
+				class="w-full h-[1px] bg-black absolute transition-all ease " 
+			></div>
             <Transition name="fade">
                 <slot v-if="state === 'edit'" name="footer"></slot>
             </Transition>
